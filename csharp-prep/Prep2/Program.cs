bb2df759ba234f3a9257a788db82af42a4cf0efa
25aajones <25averyjones@gmail.com>
2025-04-26 21:27:08 -0600

    Homework 1 Done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Part 1: Ask grade percentage & if-elif-else for letter grade w/ print statements
+        Console.WriteLine("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradePercentage = int.Parse(grade);
+        //Part 3: Add letter variable and change that instead of print statements
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        string plus_or_minus = "";
+        if (gradePercentage >= 93 || gradePercentage <= 59) {
+            plus_or_minus = "";
+        }
+        else if (gradePercentage%10 >=7) {
+            plus_or_minus = "+";
+        }
+        else if (gradePercentage%10 < 3) {
+            plus_or_minus = "-";
+        }
+        else {
+            plus_or_minus = "";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{plus_or_minus}");
+        
+        //Part 2 Passed or not
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed the class");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. You can do it next time");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-    }
+        //Console.WriteLine("Hello Sandbox World!");
+        //This is a test
+        //This is a second change.
+        Console.Write("Please input your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Please input your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your first name is: {lastName} {firstName}");
+        }
 }
