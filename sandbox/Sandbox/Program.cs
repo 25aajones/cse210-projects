1a82c85c6c22b64216cede4021ef08d6406a3112
25aajones <25averyjones@gmail.com>
2025-05-08 23:19:28 -0600

    Homework 3 Done

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,29 +1,29 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
 
+
 class Program
 {
-    static void Main(string[] args)
-    {
-        //Console.WriteLine("Hello Sandbox World!");
-        //This is a test
-        //This is a second change.
-        // Console.Write("Please input your first name: ");
-        // string firstName = Console.ReadLine();
-        // Console.Write("Please input your last name: ");
-        // string lastName = Console.ReadLine();
-        // Console.WriteLine($"Your first name is: {lastName} {firstName}");
-    
-     int x = 10;
-     if (x == 10)
-     {
-        Console.WriteLine("X is 10");
-     }
+   static void Main(string[] args)
+   {
+      // Console.WriteLine("Bonjour tout le monde");
 
-     for (int i = 0; i < x; i++)
-     {
-        Console.WriteLine($"Bob is cool: {i}");
-     }
+      Circle myCircle = new Circle();
+      Circle myCircle2 = new Circle();
+      myCircle.SetRadius(10);
+      // myCircle._radius= 10;
 
-    }
+      myCircle2.SetRadius(20);
+      // myCircle._radius= 10;
+      Console.WriteLine($"{myCircle.GetRadius()}");
+      Console.WriteLine($"{myCircle2.GetRadius()}");
+
+      Console.WriteLine($"{myCircle.GetArea()}");
+      Console.WriteLine($"{myCircle2.GetArea()}");
+
+      Cylinder myCylinder = new Cylinder();
+      myCylinder.SetCircle(myCircle);
+      myCylinder.SetHeight(10);
+      Console.WriteLine($"{myCylinder.GetVolume()}");
+   }
 }
