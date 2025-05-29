efca9e0530cb3d90908cca141855d42b3c68abed
25aajones <25averyjones@gmail.com>
2025-05-29 09:14:35 -0600

    new wordCounter and TestApp

---
+++ WordCounter/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        WordCounter wordCounter = new WordCounter("The quick brown fox jumps over the lazy dog");
+        wordCounter.DisplayWords();
+    }
+}
+
+
