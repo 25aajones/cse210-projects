9b53e4c2a66a45ae4c0afd79720feb124f33f429
25aajones <25averyjones@gmail.com>
2025-06-03 09:56:31 -0600

    Make the Person class and other stuff for it

---
+++ Person/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person myPerson = new Person("Bubba", "Bob", 53);
+
+        Console.WriteLine(myPerson.GetPersonInformation());
+
+        PoliceMan myPoliceMan = new PoliceMan("Cooper", "Silver", 34);
+        Console.WriteLine(myPoliceMan.GetPersonInformation());
+    }
+}
+
+
